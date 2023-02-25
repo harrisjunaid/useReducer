41f17789b36125f7f90fbf12d646e454d931f61f
harrisjunaid <51511122+harrisjunaid@users.noreply.github.com>
2023-02-25 11:34:10 +0000

    checked/not-checked

--- use-reducer/src/index.js
+++ use-reducer/src/index.js
@@ -4,11 +4,20 @@ import "./styles.css";
 
 // import App from "./App";
 function App() {
-  const [number, setNumber] = useReducer(
-    (number, newNumber) => number + newNumber,
-     0
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
   );
-  return <h1 onClick={() => setNumber(1)}>{number}</h1>;
+  return (
+    <>
+     <input
+      type="checkbox"
+      value={checked}
+      onChange={toggle}
+      />
+      {checked ? "checked" : "not checked"}
+    </>
+  )
 }
 
 const root = createRoot(document.getElementById("root"));
